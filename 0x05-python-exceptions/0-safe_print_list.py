4bc6b44c2e52772cdc26085f1e079618810ca751
Leulseged <leulbekele191@gmail.com>
2022-07-11 00:04:50 +0300

    safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     var = 0
 
     for i in my_list[:x]:
-        try:    
+        try:
             print(i, end='')
             var += 1
         except Exception as e:
