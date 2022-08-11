fd12dc20e11ac36e7cfc74f2a309dc5058f0acfe
Leulseged <leulbekele191@gmail.com>
2022-08-11 23:25:47 +0300

    select best

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- lists all records with a score >= 10 in second_table
-select score,name from second_table where score >= 10
-order by score desc;
+SELECT score,name FROM second_table WHERE score >= 10
+ORDER BY score DESC;
