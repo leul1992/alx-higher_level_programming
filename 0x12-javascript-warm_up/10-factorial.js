089e2cf15e45f435a208ce06a6ec89bae1237b34
Leulseged <leulbekele191@gmail.com>
2022-09-05 22:30:41 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 const num = parseInt(process.argv[2]);
 if (num && num >= 1) {
   let number = 1;
-  for (let i = 1; i <= num; i++) {
+  for (let i = 2; i <= num; i++) {
     number *= i;
   }
   console.log(number);
