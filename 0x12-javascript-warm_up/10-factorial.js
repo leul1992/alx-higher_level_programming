4ee04fb4e7ae1841f0df1fe969814ed62d281dea
Leulseged <leulbekele191@gmail.com>
2022-09-05 22:34:17 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
-if (num && num >= 1) {
-  let number = 1;
-  for (let i = 2; i <= num; i++) {
-    number *= i;
-  }
-  console.log(number);
-} else {
-  console.log(1);
+function factorial (number) {
+  if (number === 1 || isNaN(number)) { return 1; } else { return number * factorial(number - 1); }
 }
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
