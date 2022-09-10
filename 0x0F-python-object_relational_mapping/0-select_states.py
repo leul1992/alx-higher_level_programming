f8398df3d4ad88a4763f589d823b3e389ce44110
Leulseged <leulbekele191@gmail.com>
2022-09-10 13:37:11 +0300

    list all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    userName = sys.argv[1]
+    password = sys.argv[2]
+    dbName = sys.argv[3]
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=userName, passwd=password, db=dbName)
+    c = conn.cursor()
+    c.execute("""SELECT * FROM states ORDERBY states.id ASC""")
+    c.fetchall()
