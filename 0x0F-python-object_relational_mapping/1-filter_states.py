711524cd450737d3195e5f218c46b940296b7ff6
Leulseged <leulbekele191@gmail.com>
2022-09-11 21:26:46 +0300

    filter with name start with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,6 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host='localhost', port=3306, user=un,
                            passwd=pas, db=dbN)
     c = conn.cursor()
-    c.execute('SELECT * FROM states WHERE '
-              'REGEXP_LIKE(name, "^N", "c") ORDER BY id')
+    c.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id')
     for res in c:
         print(res)
