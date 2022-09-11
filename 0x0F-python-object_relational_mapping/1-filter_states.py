7f44cba24232da8e347a91e7640476be10d11531
Leulseged <leulbekele191@gmail.com>
2022-09-11 21:19:30 +0300

    filter with name start with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""filter and list states name starting with N"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    un = sys.argv[1]
+    pas = sys.argv[2]
+    dbN = sys.argv[3]
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=un,
+                           passwd=pas, db=dbN)
+    c = conn.cursor()
+    c.execute('SELECT * FROM states WHERE name LIKE "N%"')
+    for res in c:
+        print(res)
