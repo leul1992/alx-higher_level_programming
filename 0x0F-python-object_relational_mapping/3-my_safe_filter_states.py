eb189a0cf79ff00bfbcf07605a6e819b78324af5
Leulseged <leulbekele191@gmail.com>
2022-09-12 17:05:47 +0300

    safe filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""filter states by user input safely from sql injection"""
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+    conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    c = conn.cursor()
+    c.execute('SELECT * FROM states WHERE name \
+              LIKE BINARY "{}" ORDER BY id'.format(argv[4]))
+    for res in c:
+        print(res)
