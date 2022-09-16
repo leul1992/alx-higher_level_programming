6f9c26b717702e63dd54e367c5059ce088429030
Leulseged <leulbekele191@gmail.com>
2022-09-16 23:11:17 +0300

    print one obj

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import State, Base
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    for state in session.query(State).filter(State.name.contains('a')):
+        print(f'{state.id}: {state.name}')
+    session.close()
