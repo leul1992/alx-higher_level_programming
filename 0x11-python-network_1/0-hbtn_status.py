16dbea39bb8a1cd49c7348bcade72ef33864df74
Leulseged <leulbekele191@gmail.com>
2022-09-27 19:06:06 +0300

    fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,10 @@
 """fetches url and display body of the response"""
 if __name__ == '__main__':
     import urllib.request
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    req = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(req) as response:
-        the_page = response.read()
+        res = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(res)}")
+        print(f"\t- content: {res}")
+        print(f"\t-utf8 content: {res.decode('utf8')}")
