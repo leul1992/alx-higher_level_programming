90c6c73863c613e4b574a206e692b733b8dcae80
Leulseged <leulbekele191@gmail.com>
2022-09-27 23:22:59 +0300

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -19,7 +19,7 @@ def find_peak(list_of_integers):
                 return list_of_integers[1]
         mid = int(length / 2)
         if (list_of_integers[mid] > list_of_integers[mid + 1] and
-            list_of_integers[mid] > list_of_integers[mid - 1]):
+                list_of_integers[mid] > list_of_integers[mid - 1]):
             return list_of_integers[mid]
         else:
             if(list_of_integers[mid] < list_of_integers[mid + 1]):
