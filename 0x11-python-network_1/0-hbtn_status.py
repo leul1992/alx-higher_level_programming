4d3bb66caf253b765bb199a04114e0a42a208ed0
Leulseged <leulbekele191@gmail.com>
2022-09-27 19:19:46 +0300

    fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
         print("Body response:")
         print(f"\t- type: {type(res)}")
         print(f"\t- content: {res}")
-        print(f"\t-utf8 content: {res.decode('utf8')}")
+        print(f"\t- utf8 content: {res.decode('utf8')}")
