0b9e8b2b9ae1c19141881c186040223d182ca0c3
Leulseged <leulbekele191@gmail.com>
2022-09-27 10:40:18 +0300

    supported methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # display all http methods the server will accept
-curl -si -L -X OPTIONS "$1" | grep "Allow" | cut -d ":" -f2-
+curl -si -L -X OPTIONS "$1" | grep "Allow" | cut -d ":" -f3-
